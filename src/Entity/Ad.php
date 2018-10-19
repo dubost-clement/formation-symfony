9ce4765fb281687527b57be14884e4bee456aade
DUBOST Clément <braunschweig-dubost@outlook.fr>
2018-10-19 10:37:44 +0200

    Ajout contraintes de validation pour les formulaires

--- src/Entity/Ad.php
+++ src/Entity/Ad.php
@@ -6,10 +6,16 @@ use Cocur\Slugify\Slugify;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\AdRepository")
  * @ORM\HasLifecycleCallbacks
+ * @UniqueEntity(
+ *  fields={"title"},
+ *  message="Une autre annonce comporte le même titre, merci de le modifier"
+ * )
  */
 class Ad
 {
@@ -22,6 +28,12 @@ class Ad
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(
+     *  min = 10,
+     *  max = 255,
+     *  minMessage = "Le titre doit faire plus de {{ limit }} caractères !",
+     *  maxMessage = "Le titre ne doit pas faire plus de {{ limit }} caractères !"
+     *)
      */
     private $title;
 
@@ -37,11 +49,19 @@ class Ad
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\Length(
+     *  min = 20,
+     *  minMessage = "Votre introduction doit faire plus de {{ limit }} caractères !",
+     *)
      */
     private $introduction;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\Length(
+     *  min = 100,
+     *  minMessage = "Votre description doit faire plus de {{ limit }} caractères !",
+     *)
      */
     private $content;
 
