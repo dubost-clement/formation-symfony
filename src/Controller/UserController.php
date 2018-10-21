2ab992c881159e1e7c7cced9b482e9484a8e6218
dubost-clement <braunschweig-dubost@outlook.fr>
2018-10-22 00:20:14 +0200

    Mise en place de la page affichage utilisateur

---
+++ src/Controller/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class UserController extends Controller
+{
+    /**
+     * Permet d'afficher un utilisateur
+     * @Route("/user/{slug}", name="user_show")
+     */
+    public function index(User $user)
+    {
+        return $this->render('user/index.html.twig', [
+            'user' => $user
+        ]);
+    }
+}
