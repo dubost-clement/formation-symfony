2b1ec86f9089507480d0315f316de3fd523d8a3a
dubost-clement <braunschweig-dubost@outlook.fr>
2018-10-28 21:27:25 +0100

    Mise en place de la liste  des annonces et des utilisateurs

---
+++ src/Controller/AdminUserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminUserController extends AbstractController
+{
+    /**
+     * Permet d'afficher la liste des utilisateurs
+     * @Route("/admin/users", name="admin_users_index")
+     */
+    public function index(UserRepository $repo)
+    {
+        return $this->render('admin/user/index.html.twig', [
+            'users' => $repo->findAll()
+        ]);
+    }
+}
