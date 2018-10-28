de5f964d399533747121e2838a0d0ccabc10c8c4
dubost-clement <braunschweig-dubost@outlook.fr>
2018-10-28 19:18:14 +0100

    Mise en place de la modification et suppression des commentaires

--- src/Controller/AdminCommentController.php
+++ src/Controller/AdminCommentController.php
@@ -2,13 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Form\AdminCommentType;
 use App\Repository\CommentRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminCommentController extends AbstractController
 {
     /**
+     * Permet d'afficher la liste des commentaires
      * @Route("/admin/comments", name="admin_comments_index")
      */
     public function index(CommentRepository $repo)
@@ -16,5 +21,46 @@ class AdminCommentController extends AbstractController
         return $this->render('admin/comment/index.html.twig', [
             'comments' => $repo->findAll()
         ]);
+    }
+
+    /**
+     * Permet d'afficher un commentaire
+     * @Route("/admin/comments/{id}/edit", name="admin_comments_edit")
+     */
+    public function edit(Comment $comment, Request $request, ObjectManager $manager)
+    {
+        $form = $this->createForm(AdminCommentType::class, $comment);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($comment);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Le commentaire a bien été modifié !'
+            );
+        }
+
+        return $this->render('admin/comment/edit.html.twig', [
+            'comment' => $comment,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet de supprimer un commentaire
+     * @Route("/admin/comments/{id}/delete", name="admin_comments_delete")
+     */
+    public function delete(Comment $comment, ObjectManager $manager)
+    {
+        $manager->remove($comment);
+        $manager->flush();
+    
+        $this->addFlash(
+            'success',
+            'le commentaire a bien été supprimée !'
+        );
+
+        return $this->redirectToRoute('admin_comments_index');
     }
 }
