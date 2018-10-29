a744499eb99cc65128c77855ccee0fd685d23d30
dubost-clement <braunschweig-dubost@outlook.fr>
2018-10-29 12:19:41 +0100

    Mise en place mofications et suppression d'une réservation. Ajout groupe de validation pour le formulaire de réservation Front

--- src/Controller/AdminBookingController.php
+++ src/Controller/AdminBookingController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Booking;
 use App\Form\AdminBookingType;
 use App\Repository\BookingRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -25,13 +27,44 @@ class AdminBookingController extends AbstractController
      * Permet de modifier une réservation
      * @Route("/admin/bookings/{id}/edit", name="admin_bookings_edit")
      */
-    public function edit(Booking $booking)
+    public function edit(Booking $booking, Request $request, ObjectManager $manager)
     {
         $form = $this->createForm(AdminBookingType::class, $booking);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $booking->setAmount(0);
+
+            $manager->persist($booking);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'La modification de la réservation a bien été enregistrée'
+            );
+
+            return $this->redirectToRoute("admin_bookings_index");
+        }
 
         return $this->render('admin/booking/edit.html.twig', [
             'form' => $form->createView(),
             'booking' => $booking
         ]);
+    }
+
+    /**
+     * Permet de supprimer une réservation
+     * @Route("/admin/bookings/{id}/delete", name="admin_bookings_delete")
+     */
+    public function delete(Booking $booking, ObjectManager $manager)
+    {
+        $manager->remove($booking);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            'La réservations a bien été supprimée !'
+        );
+
+        return $this->redirectToRoute("admin_bookings_index");
     }
 }
