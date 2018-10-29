e4d9d77d3fcc4f66ce34425f77c0130c5e585778
dubost-clement <braunschweig-dubost@outlook.fr>
2018-10-29 14:27:31 +0100

    Mise en place de la modification d'un utilisateur

--- src/Controller/AdminUserController.php
+++ src/Controller/AdminUserController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\AccountType;
 use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -16,6 +20,32 @@ class AdminUserController extends AbstractController
     {
         return $this->render('admin/user/index.html.twig', [
             'users' => $repo->findAll()
+        ]);
+    }
+
+    /**
+     * Permet de modifier un utilisateur
+     * @Route("/admin/users/{id}/edit", name="admin_users_edit")
+     */
+    public function edit(User $user, Request $request, ObjectManager $manager)
+    {
+        $form = $this->createForm(AccountType::class, $user);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Vos modifications ont été enregistrées !'
+            );
+        }
+
+        return $this->render('admin/user/edit.html.twig', [
+            'form' => $form->createView(),
+            'user' => $user
         ]);
     }
 }
