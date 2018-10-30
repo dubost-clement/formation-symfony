d68248def0c3fdb280bf1fe0163771d7f2dfa7df
dubost-clement <braunschweig-dubost@outlook.fr>
2018-10-30 11:34:56 +0100

    Mise en place Service stats pour le dashboard

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -2,18 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\Stats;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminDashboardController extends AbstractController
 {
     /**
      * @Route("/admin", name="admin_dashboard")
      */
-    public function index()
+    public function index(ObjectManager $manager, Stats $service)
     {
+        $stats = $service->getStats();
+        $bestAds = $service->getAdsStats('DESC');
+        $worstAds = $service->getAdsStats('ASC');
+
         return $this->render('admin/dashboard/index.html.twig', [
-            'controller_name' => 'AdminDashboardController',
+            'stats' => $stats,
+            'best' => $bestAds,
+            'worst' => $worstAds
         ]);
     }
 }
